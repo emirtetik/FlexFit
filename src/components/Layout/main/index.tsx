20a50797d79163fb241a6b5f2db3d7cc183bc3ab
Emir Tetik <emirtetik.41@hotmail.com>
2025-05-30 15:11:01 +0300

    folder error fixed

--- src/components/Layout/main/index.tsx
+++ src/components/Layout/main/index.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../Navigation/header";
 import Footer from "../../Navigation/footer";
-
 const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen ">
